cb0ed6c6ac34045fb3d90bed9957a4d6a051891e
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 16:07:38 +0700

    Tugas 19 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas19.js
@@ -0,0 +1,15 @@
+function Tugas19() {
+  var Text = "Semangat...";
+  var Text2 = "*";
+  console.log(Text.repeat(5));
+  console.log(Text2.repeat(5));
+  console.log(Text2.repeat(25));
+  console.log(Text2.repeat(45));
+  console.log(Text2.repeat(65));
+  console.log(Text2.repeat(95));
+  console.log(Text2.repeat(65));
+  console.log(Text2.repeat(45));
+  console.log(Text2.repeat(25));
+  console.log(Text2.repeat(5));
+}
+Tugas19();
